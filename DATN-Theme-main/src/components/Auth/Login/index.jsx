362d9ffc163d440e5cab04442c694c9796bf0de2
hung dinh <so5so6vaso9@gmail.com>
2025-01-13 18:31:45 +0700

    xóa ghi nhớ

--- DATN-Theme-main/src/components/Auth/Login/index.jsx
+++ DATN-Theme-main/src/components/Auth/Login/index.jsx
@@ -202,14 +202,7 @@ export default function Login() {
 
               <div className="flex justify-between items-center mb-6">
                 <div className="flex items-center space-x-2">
-                  <input
-                    type="checkbox"
-                    id="remember-me"
-                    className="w-5 h-5 text-gray-500 border-gray-300 rounded focus:ring-2 focus:ring-gray-500"
-                  />
-                  <label htmlFor="remember-me" className="text-sm text-gray-600">
-                    Ghi nhớ
-                  </label>
+                    
                 </div>
                 <a href="/forgot-password" className="text-sm text-red-500 hover:underline">
                   Quên mật khẩu?
